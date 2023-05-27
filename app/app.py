b852bb9fd002c6cd8ad792b7a74c71b350d9bcba
shi <lehkost16@outlook.com>
2023-05-28 04:54:16 +0800

    test app

--- app/app.py
+++ app/app.py
@@ -33,7 +33,7 @@ tasks = [
 @app.route('/todo/api/v1.0/tasks', methods=['get'])
 def get_tasks():
     return jsonify({'tasks': tasks})
-@app.route('/todo/api/v1.0/task/<int:task_id>'.methons=['get'])
+@app.route('/todo/api/v1.0/task/<int:task_id>',methons=['get'])
 def get_task():
     task =filter(lambda t:t['id']== task_id,tasks)
     if len(task) ==0:
