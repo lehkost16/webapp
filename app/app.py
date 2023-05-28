ab39cc530d868507416373624d5a6eea6e84a761
shi <lehkost16@outlook.com>
2023-05-28 21:21:17 +0800

    test app

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
-conn =pymysql.connect(host='my_mysql',port=3307,database='t_d',user='root',password='1201',charset='utf8')
+conn =pymysql.connect(host='my_mysql',port=33060,database='t_d',user='root',password='1201',charset='utf8')
 cursor =conn.cursor()
 
 
