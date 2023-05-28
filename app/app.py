3755dc2f3bae9036b9fd204c2136fe6f406c467b
shi <lehkost16@outlook.com>
2023-05-28 15:33:55 +0800

    test app

--- app/app.py
+++ app/app.py
@@ -4,14 +4,14 @@ from mysql import connector
 
 app = Flask(__name__)
 
-# config = {
-#         'user': 'root',
-#         'password': '1201',
-#         'host': '172.26.28.185',
-#         'port': '3306',
-#         'database': 'database'
-#     }
-# connection = mysql.connector.connect(**config)
+config = {
+        'user': 'root',
+        'password': '1201',
+        'host': '172.26.28.185',
+        'port': '3306',
+        'database': 'database'
+    }
+connection = mysql.connector.connect(**config)
 
 
 tasks = [
